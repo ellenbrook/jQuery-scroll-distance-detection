5fc06d41a872a45afb49c9340c0f030475e16401
Sakto <daniel@danwebs.dk>
2014-09-10 14:19:45 +0200

    Changed to jQuery plugin format

--- scroll-detection.js
+++ scroll-detection.js
@@ -8,33 +8,33 @@
 //
 //##############################################################
 
-$(document).ready(function(){
+$.fn.detectScrollDistance = function(targetPercentage, callback){
+	
+	var self = this;
+	
 	$(window).scroll(function(){
-		
-		// Change this to target a different percentage
-		var targetPercentage = 70;
-		//Change this to set the height of your nav bar so it hides properly. IF you have a box shadow you may have to adjust this number to be height + shadow distance
-		var navBarHeight = 66;
-		//Change this to the ID of the content you are trying to show.
-		var targetID = "#navigation";
-		
-		//Window Math
 		var scrollTo = $(window).scrollTop(),
 		docHeight = $(document).height(),
 		windowHeight = $(window).height();
 		scrollPercent = (scrollTo / (docHeight-windowHeight)) * 100;
 		scrollPercent = scrollPercent.toFixed(1);
-			
-		$('#percentageCounter h1').text(scrollPercent+"%");
-
+		
+		var navBarHeight = $(this).height();
+		
 		if(scrollPercent > targetPercentage) {
-			$(targetID).css({ top: '0' });
+			self.css({ top: '0' });
 		}
 		
 		if(scrollPercent < targetPercentage) {
-			$(targetID).css({ top: '-'+navBarHeight+'px' });
+			self.css({ top: '-'+navBarHeight+'px' });
 		}
-								
-	}).trigger('scroll');
+		
+	});
+	
+};
+
+$(document).ready(function(){
+	
+	$('#navigation').detectScrollDistance(70);
 	
 });
