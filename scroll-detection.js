f85432cc41b64e0752fae74146585e32a00e984f
Eric Ellenbrook <ericellenbrook@gmail.com>
2014-10-09 19:07:03 -0400

    Proper jQuery Plugin

--- scroll-detection.js
+++ scroll-detection.js
@@ -8,58 +8,142 @@
 //
 //##############################################################
 
-/**
- * @param {Object} targetPercentage
- * @param {Object} scrollIn - callback for when the DOM element reaches targetPercentage
- * @param {Object} scrollOut - callback for when the DOM element is no longer at targetPercentage
- * @param {Object} scrollEventCallback - Callback for every time the scroll event is triggered.
- */
-$.fn.detectScrollDistance = function(targetPercentage, scrollIn, scrollOut, scrollEventCallback){
-	
-	var self = this;
-	
-	$(window).on('scroll', function(){
-		var scrollTo = $(window).scrollTop(),
-		docHeight = $(document).height(),
-		windowHeight = $(window).height();
-		scrollPercent = (scrollTo / (docHeight-windowHeight)) * 100;
-		scrollPercent = scrollPercent.toFixed(1);
-		
-		// Are we there yet?
-		if(scrollPercent > targetPercentage)
-		{
-			if(typeof scrollIn == 'function')
-			{
-				// We've reached the target
-				scrollIn.call(self, scrollPercent);
-			}
-			else
-			{
-				// Was a callback function supplied? If not, then this is pointless.. Throw an exception
-				throw('You need to supply a callback function to detectScrollDistance');
-			}
-		}
-		
-		// Scrolling out of view
-		if(scrollPercent < targetPercentage)
-		{
-			if(typeof scrollOut == 'function')
-			{
-				scrollOut.call(self, scrollPercent);
-			}
-		}
-		
-		// The scroll event fired, call the callback function, if one was supplied.
-		if(typeof scrollEventCallback == 'function')
-		{
-			// Set the context of "this" to self (the object detectScrollDistance was called on ((in the example "this" would refer to #navigation)))
-			// If we don't do .call(self), "this" will refer to the window object in the callback function.
-			scrollEventCallback.call(self, scrollPercent);
-		}
-		
-	});
-	
-	return self;
-	
-};
+(function($) {
 
+    $.fn.detectScroll = function(options) {
+
+    	var settings = $.extend({
+            position     : "fade",
+            target       : "#targetContainer",
+            size         : null,
+            testMode     : false,
+            targetPercent: 70,
+            objectHeight: null,
+            objectWidth: null,
+            speed: 400
+        }, options);
+
+
+        return this.each( function() 
+        {
+			var self = this;
+
+	        $( this ).on( 'scroll', function()
+	        {
+	        	this.scrollTo = $( window ).scrollTop();
+				this.docHeight = $( document ).height();
+				this.windowHeight = $( window ).height();
+				this.scrollPercent = ( scrollTo / ( docHeight-windowHeight ) ) * 100;
+				this.scrollPercent = scrollPercent.toFixed( 1 );
+
+				//Do the scroll detection work
+				if( this.scrollPercent > settings.targetPercent )
+				{
+					// Did they want it faded in?
+				    if( settings.position == "fade")
+				    {
+				    	$( settings.target ).fadeIn( settings.speed );
+				    }
+
+				    // Did they position it from the top?
+				    if( settings.position == "top")
+				    {
+				    	$( settings.target ).stop().animate( {top: "0"}, settings.speed );
+				    }
+
+				    // Did they position it from the right?
+				    if( settings.position == "right")
+				    {
+				    	$( settings.target ).stop().animate( {right: "0"}, settings.speed );
+				    }
+
+				    // Did they position it from the bottom?
+				    if( settings.position == "bottom")
+				    {
+				    	$( settings.target ).stop().animate( {bottom: "0"}, settings.speed );
+				    }
+
+				    // Did they position it from the left?
+				    if( settings.position == "left" )
+				    {
+				    	$( settings.target ).stop().animate( {left: "0"}, settings.speed );
+				    }
+				}
+			
+				if( this.scrollPercent < settings.targetPercent ) 
+				{
+					// Did they want it faded in?
+				    if( settings.position == "fade" )
+				    {
+				    	$( settings.target ).fadeOut( settings.speed );
+				    }
+
+				    // Did they position it from the top?
+				    if( settings.position == "top" )
+				    {
+				    	$( settings.target ).stop().animate( {top: "-"+settings.height+"px"}, settings.speed );
+				    }
+
+				    // Did they position it from the right?
+				    if( settings.position == "right" )
+				    {
+				    	$( settings.target ).stop().animate( {right: "-"+settings.width+"px"}, settings.speed );
+				    }
+
+				    // Did they position it from the bottom?
+				    if( settings.position == "bottom" )
+				    {
+				    	$( settings.target ).stop().animate( {bottom: "-"+settings.height+"px"}, settings.speed );
+				    }
+
+				    // Did they position it from the left?
+				    if( settings.position == "left" )
+				    {
+				    	$( settings.target ).stop().animate( {left: "-"+settings.width+"px"}, settings.speed );
+				    }
+				}
+
+				// If testMode is on then update the counter in real time
+			    if ( settings.testMode ) 
+			    {
+		        	$( '#detectScrollTestView' ).html( scrollPercent+"%" );
+		    	} // testMode
+
+			    return self;
+
+	    	}); // on scrolll
+
+		    // Did they position it from the bottom?
+		    if( settings.position == "bottom")
+		    {
+		    	//do something from the bottom
+		    }
+
+		    // Did they position it from the left?
+		    if( settings.position == "left")
+		    {
+		    	//do something from the top
+		    }
+
+		    // Is testMode on? Better show a percentage counter
+		    if ( settings.testMode ) 
+		    {
+		        $( 'body' ).append( '<div id="detectScrollTestView">Scroll</div>' );
+		        $( '#detectScrollTestView' ).css({
+		        	position:'fixed',
+		        	bottom:'20px',
+		        	right:'30px',
+		        	background:'rgba(44, 62, 80,.25)',
+		        	width: '48px',
+		        	height: '48px',
+		        	'font-size':'18px',
+		        	'line-height': '42px',
+		        	'text-align': 'center',
+		        	borderRadius: '50%',
+		        	padding: '16px',
+		        });
+	    	} // End of testMode
+		});
+    }
+
+}(jQuery));
